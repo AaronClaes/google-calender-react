5124f9f272aad5ee68c206de4e90a8d8b07cdcee
Aaron Claes <aaronclaes22@gmail.com>
2021-10-16 15:52:42 +0200

    fix: fix className items-center typo

--- src/components/CalenderHeader.js
+++ src/components/CalenderHeader.js
@@ -21,7 +21,7 @@ function CalenderHeader() {
   }
 
   return (
-    <header className="px-4 py-2 flex item-center">
+    <header className="px-4 py-2 flex items-center">
       <img src={logo} alt="calender logo" className="mr-2 w-12 h-12" />
       <h1 className="mr-10 text-xl text-gray-500 font-bold">Calender</h1>
       <button onClick={handleReset} className="border rounded py-2 px-4 mr-5">
