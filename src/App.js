01ed1a02fda7275476e3f768d4231db68bd42ca3
Aaron <aaronclaes22@gmail.com>
2021-10-19 15:03:36 +0200

    feat: add desktop only

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import CalendarScreen from "./screens/CalendarScreen";
@@ -9,24 +9,39 @@ import HomeScreen from "./screens/HomeScreen";
 import GithubLink from "./components/GithubLink";
 import TestDatabaseScreen from "./screens/TestDatabaseScreen";
 
+import {
+  BrowserView,
+  MobileView,
+  isBrowser,
+  isMobile,
+} from "react-device-detect";
+import DesktopOnly from "./screens/DesktopOnly";
+
 function App() {
   return (
-    <AuthWrapper>
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <HomeScreen />
-          </Route>
-          <Route path="/database">
-            <TestDatabaseScreen />
-          </Route>
-          <PrivateRoute path="/calendar">
-            <CalendarScreen />
-          </PrivateRoute>
-        </Switch>
-        <GithubLink />
-      </Router>
-    </AuthWrapper>
+    <Fragment>
+      <MobileView>
+        <DesktopOnly />
+      </MobileView>
+      <BrowserView>
+        <AuthWrapper>
+          <Router>
+            <Switch>
+              <Route exact path="/">
+                <HomeScreen />
+              </Route>
+              <Route path="/database">
+                <TestDatabaseScreen />
+              </Route>
+              <PrivateRoute path="/calendar">
+                <CalendarScreen />
+              </PrivateRoute>
+            </Switch>
+            <GithubLink />
+          </Router>
+        </AuthWrapper>
+      </BrowserView>
+    </Fragment>
   );
 }
 
---
+++ src/screens/DesktopOnly.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function DesktopOnly() {
+  return <div>DESKTOP ONLY!</div>;
+}
+
+export default DesktopOnly;
