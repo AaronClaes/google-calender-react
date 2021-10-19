5505f0641c41f26bd40aec8886be7d8d4ed46974
Aaron Claes <aaronclaes22@gmail.com>
2021-10-19 20:15:48 +0200

    feat: add desktop only page for mobile

--- src/screens/DesktopOnly.js
+++ src/screens/DesktopOnly.js
@@ -1,7 +1,28 @@
 import React from "react";
+import { useHistory } from "react-router";
 
 function DesktopOnly() {
-  return <div>DESKTOP ONLY!</div>;
+  const history = useHistory();
+  return (
+    <div className="pt-60 m-5">
+      <h1 className="text-4xl text-gray-700 font-bold text-center">
+        Google Calendar Clone
+      </h1>
+      <p className="text-xl text-gray-700 pt-4 text-center">
+        This app is currently only usable on desktop.
+      </p>
+      <div className="w-100 flex justify-center">
+        <button
+          onClick={() =>
+            (window.location.href = "https://github.com/AaronClaes")
+          }
+          className="border rounded py-2 px-6 mr-5 mt-5 bg-blue-500 text-white hover:bg-blue-600"
+        >
+          My Github
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default DesktopOnly;
